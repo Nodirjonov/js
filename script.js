0de8e5147ae99dcba7d514d1469e5ded05128141
Nodirjonov <sobirovakomila85@gmail.com>
2024-02-23 17:58:25 +0500

    js

---
+++ script.js
@@ -0,0 +1,7 @@
+let a=prompt('Как вас зовут?')
+let b=prompt('Какая у вас фамилия?')
+let c=prompt('Сколько вам лет?')
+let d=prompt('Где вы живете?')
+let e=prompt('какой ваш любимый школьный предмет?')
+console.log(a,b,c,d,e);
+
